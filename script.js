43bbbbe8c737253cb23180927b173233df1799f4
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-14 19:36:05 +0100

    printed result on page html and added style css at page

--- script.js
+++ script.js
@@ -3,23 +3,41 @@
  //per i multipli di 5 stampi “Buzz” al posto del numero
  //per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero
 
+//<div class="card">
+//  <h2>12</h2>
+//</div>
+const wrapperElement = document.querySelector('div.wrapper');
+
+
  let print;
 
  for (let i = 1; i <= 100; i++){
-    console.log(i);
+    
     if (( i % 3 == 0 ) && ( i % 5 == 0 )) {
         print = "FizzBuzz";
         console.log(print);
     }
-    else if ( i % 5 == 0 ) {
-        print = "Buzz";
+    else if ( i % 3 == 0 ) {
+        print = "Fizz";
         console.log(print);
     }    
-    else if ( i % 3 == 0 ) { 
-        print = "Fizz";  
+    else if ( i % 5 == 0 ) { 
+        print = "Buzz";  
         console.log(print);
     }
     else {
-        console.log(i)
+        print = i;
+        console.log(print)
     }
+    
+    const cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    const printElement = document.createElement('h2');
+    printElement.innerHTML = print;
+
+    cardElement.appendChild(printElement);
+    wrapperElement.appendChild(cardElement);
  }
+//BONUS 1:
+//Crea un container nel DOM , aggiungendo (attraverso la funzione appendChild())
+//un elemento html con il numero o la stringa corretta da mostrare.
