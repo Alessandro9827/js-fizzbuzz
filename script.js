2dcd21d1f3987def4bc869d59f18099b4ecab50c
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-14 20:05:20 +0100

    added color for differnt results (bonus:2)

--- script.js
+++ script.js
@@ -13,31 +13,39 @@ const wrapperElement = document.querySelector('div.wrapper');
 
  for (let i = 1; i <= 100; i++){
     
-    if (( i % 3 == 0 ) && ( i % 5 == 0 )) {
-        print = "FizzBuzz";
-        console.log(print);
-    }
-    else if ( i % 3 == 0 ) {
-        print = "Fizz";
-        console.log(print);
-    }    
-    else if ( i % 5 == 0 ) { 
-        print = "Buzz";  
-        console.log(print);
-    }
-    else {
-        print = i;
-        console.log(print)
-    }
-    
+    //Creazione Cards dove verranno stampati risultati
     const cardElement = document.createElement('div');
     cardElement.className = 'card';
     const printElement = document.createElement('h2');
-    printElement.innerHTML = print;
-
+    
+    //Collega tra di loro il Div e H2
     cardElement.appendChild(printElement);
     wrapperElement.appendChild(cardElement);
+
+    if (( i % 3 == 0 ) && ( i % 5 == 0 )) {
+        print = "FizzBuzz";
+        cardElement.className = 'fizz-bazz-color';
+    }
+    else if ( i % 3 == 0 ) {
+        print = "Fizz";
+        cardElement.className = 'fizz-color';
+    }    
+    else if ( i % 5 == 0 ) { 
+        print = "Buzz";
+        cardElement.className = 'buzz-color';
+    }
+    else {
+        print = i;
+    }
+
+    //Stampa i risultati
+    printElement.innerHTML = print;
  }
+
 //BONUS 1:
 //Crea un container nel DOM , aggiungendo (attraverso la funzione appendChild())
 //un elemento html con il numero o la stringa corretta da mostrare.
+
+//BONUS 2:
+//Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il 
+//valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
